2b3c2ed359b9d51768e1d914131f3edf75d66a32
wak2luang <45333585+wak2luang@users.noreply.github.com>
2018-12-23 04:13:18 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh52Vr6qBsbWlnNyyjYegfmSny4OpdWO1iHabkI233X5li5evmH2xtaiLza2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "6ab0bf075ff6bdb5";
+$os="android";
+$sign="c5bbc9de118324d09c377fee5e481b1b1f103959fb06417c36e9f9c8584fce76";
